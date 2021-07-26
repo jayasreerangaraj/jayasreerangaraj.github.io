687d0e12901a140225a0ba01f35ca055dffeaaa2
Jayasree R <jaisri0805@gmail.com>
2021-07-26 21:50:38 +0530

    updated new

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jayasreerangaraj
+ protoflio
---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+    debugger;
+    $('.clsUserIcon').on('click', function (e) {
+        debugger;
+        $(this).toggleClass('rounded-circle blue');
+        $('.clsHeading').slice(1, 5).toggleClass('bg');
+    });
+});
+
+
